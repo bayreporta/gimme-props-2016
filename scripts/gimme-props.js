d56518f8dfb45118254f387c76b32fe30f19e211
John Osborn <bayreporta@gmail.com>
2016-10-10 16:45:31 -0700

    prop header

--- scripts/gimme-props.js
+++ scripts/gimme-props.js
@@ -1,8 +1,15 @@
 /* GLOBAL VARIABLES
 --------------------------------------------------------*/
-var curSlide = '#nocookie', rightSlide = '#proplist', leftSlide = null, upSlide = null, downSlide = null, propSlide = null;
-var propSelected = null, propID, tutorial = false, lockSlide = false, propMode = false, firstPropPass;
+var curSlide = '#intro', rightSlide = '#about', leftSlide = null, upSlide = null, downSlide = null, propSlide = null;
+var propSelected = null, propID, tutorial = false, lockSlide = false, propMode = false, returning, firstPropPass;
 var propData = new Object, points;
+var propTitles = {
+	'prop59': {
+		name: 'Prop 59',
+		color: '#c5c558',
+	 	icon:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><rect x="5" y="5" width="90" height="90" fill="none"/><path d="M89.6 33.3c-2.2-5.1-5.3-9.7-9.2-13.7 -3.9-3.9-8.5-7-13.7-9.2C61.4 8.2 55.8 7.1 50 7.1s-11.4 1.1-16.7 3.4c-5.1 2.2-9.7 5.3-13.7 9.2 -3.9 3.9-7 8.5-9.2 13.7 -2.2 5.3-3.4 10.9-3.4 16.7 0 5.8 1.1 11.4 3.4 16.7 2.2 5.1 5.3 9.7 9.2 13.7 3.9 3.9 8.5 7 13.7 9.2 5.3 2.2 10.9 3.4 16.7 3.4s11.4-1.1 16.7-3.4c5.1-2.2 9.7-5.3 13.7-9.2s7-8.5 9.2-13.7c2.2-5.3 3.4-10.9 3.4-16.7C93 44.2 91.8 38.6 89.6 33.3zM87.5 65.9c-2 4.8-5 9.2-8.7 12.9 -3.7 3.7-8.1 6.7-12.9 8.7 -5 2.1-10.3 3.2-15.8 3.2 -5.5 0-10.8-1.1-15.8-3.2 -4.8-2-9.2-5-12.9-8.7 -3.7-3.7-6.7-8.1-8.7-12.9 -2.1-5-3.2-10.3-3.2-15.8s1.1-10.8 3.2-15.8c2-4.8 5-9.2 8.7-12.9 3.7-3.7 8.1-6.7 12.9-8.7C39.2 10.5 44.5 9.4 50 9.4s10.8 1.1 15.8 3.2c4.8 2 9.2 5 12.9 8.7 3.7 3.7 6.7 8.1 8.7 12.9 2.1 5 3.2 10.3 3.2 15.8S89.6 60.8 87.5 65.9z"/><path d="M40.6 37.8c0 4.2 2.7 7.8 6.7 9l0-5.7 -0.1-12.3C43.3 30.1 40.6 33.7 40.6 37.8z"/><path d="M52.7 71.2c4-1.2 6.7-4.8 6.7-9 0-4.2-2.7-7.8-6.6-9L52.7 70.4V71.2z"/><path d="M76.1 23.9c-3.4-3.4-7.3-6.1-11.7-7.9 -4.6-1.9-9.4-2.9-14.4-2.9s-9.8 1-14.4 2.9c-4.4 1.9-8.3 4.5-11.7 7.9 -3.4 3.4-6.1 7.3-7.9 11.7 -1.9 4.6-2.9 9.4-2.9 14.4 0 5 1 9.8 2.9 14.4 1.9 4.4 4.5 8.4 7.9 11.7 3.4 3.4 7.3 6.1 11.7 7.9 4.6 1.9 9.4 2.9 14.4 2.9s9.8-1 14.4-2.9c4.4-1.9 8.4-4.5 11.7-7.9 3.4-3.4 6.1-7.3 7.9-11.7 1.9-4.6 2.9-9.4 2.9-14.4 0-5-1-9.8-2.9-14.4C82.2 31.3 79.5 27.3 76.1 23.9zM52.8 41.2l0 6.4c3.3 0.6 6.3 2.4 8.5 5 2.3 2.7 3.6 6.2 3.6 9.7 0 3.6-1.3 7.1-3.6 9.8 -2.2 2.6-5.3 4.3-8.6 4.9v2.6c0 1.5-1.2 2.8-2.8 2.8s-2.8-1.2-2.8-2.8v-2.6c-3.3-0.6-6.3-2.4-8.5-5 -2.3-2.7-3.6-6.2-3.6-9.7 0-1.5 1.2-2.8 2.8-2.8s2.8 1.2 2.8 2.8c0 4.1 2.7 7.7 6.6 9V70.4l0.1-17.9c-3.3-0.6-6.3-2.4-8.6-5 -2.3-2.7-3.6-6.2-3.6-9.7 0-3.6 1.3-7 3.6-9.7 2.2-2.6 5.2-4.3 8.5-5l0-2.6c0-1.5 1.2-2.8 2.8-2.8h0c1.5 0 2.8 1.2 2.8 2.8l0 2.6c3.3 0.6 6.4 2.4 8.6 5 2.3 2.7 3.6 6.2 3.6 9.8 0 1.5-1.2 2.8-2.8 2.8s-2.8-1.2-2.8-2.8c0-4.2-2.7-7.8-6.6-9L52.8 41.2z"/></svg>'
+	}
+}
 
 /* COOKIE CONTROL
 --------------------------------------------------------*/
@@ -196,30 +203,39 @@ function configureSlides(direct){
 
 		//check victory conditions
 		if (propData[propSelected][propID].victoryslide == true){
+			
+			//Add appropriate text
 			var text;
 
-			if (points < 0){text = "OPPOSE IT!"}
-			else if (points > 0){text = "SUPPORT IT!"}
-			else if (points == 0){text = "GO EITHER WAY"}
+			if (points < 0){
+				text = "OPPOSE IT!";
+				$(curSlide).addClass('darkred');
+			}
+			else if (points > 0){
+				text = "SUPPORT IT!";
+				$(curSlide).addClass('darkgreen');
+			}
+			else if (points == 0){
+				text = "GO EITHER WAY";
+				$(curSlide).addClass('darkgray');
+			}
 
 			$('<h1/>',{
-				'text': text
+				'text': text,
+				'style': 'font-size:8rem;'
 			}).prependTo(curSlide + ' .propquestion');
 
-		}
-
-		/*get back on the path man
-		if (propSlide == '#results'){
-			//return to prop in list
-			rightSlide == '#' + propSelected;
 			propMode = false;
-			//hide carets
-			$('.rightcaret').animate({opacity:.3}, 300); $('.propcaretleft').add('propcaretright').fadeOut(300);
-
+			returning = true;
 		}
-
-		curSlide = '#proplist59'; upSlide = '#proplist'; rightSlide = '#tutorial'; propSelected = 'prop59';tutorial = true;
-		$('.downcaret').add('.upcaret').add('.rightcaret').animate({opacity:.3}, 300); $('.leftcaret').animate({opacity:0}, 300);*/
+	}
+	//RETURNING TO PROP
+	else if (returning == true){   
+		curSlide = '#proplist'; downSlide = '#prop59';returning = false;
+		setTimeout(function(){
+			$('.' + propSelected).add('.propheader').remove();
+		}, 300);
+		$('.downcaret').animate({opacity:.3}, 300); $('.propcaretleft').add('.propcaretright').fadeOut(300);
 	}
 	//ABOUT
 	else if (curSlide == '#intro' && direct == 'right' || curSlide == '#whoweare' && direct == 'up'){    
@@ -254,7 +270,7 @@ function configureSlides(direct){
 		dropCookie('no');
 	}
 	//PROP LIST MAIN
-	else if (curSlide == '#yescookie' && direct == 'left' || curSlide == '#nocookie' && direct == 'right' || curSlide == '#proplist59' && direct == 'up'){   
+	else if (curSlide == '#yescookie' && direct == 'left' || curSlide == '#nocookie' && direct == 'right' || curSlide == '#prop59' && direct == 'up'){   
 		clearAnimations(); curSlide = '#proplist'; downSlide = '#prop59'; tutorial = false;
 		$('.downcaret').animate({opacity:.3}, 300); $('.rightcaret').add('.leftcaret').add('.upcaret').animate({opacity:0}, 300);
 	}
@@ -269,7 +285,6 @@ function configureSlides(direct){
 		curSlide = '#prop59'; upSlide = '#proplist'; rightSlide = '#tutorial'; propSelected = 'prop59';tutorial = true;
 		$('.downcaret').add('.upcaret').add('.rightcaret').animate({opacity:.3}, 300); $('.leftcaret').animate({opacity:0}, 300);
 	}	
-
 
 	//configure next slides
 	$(upSlide).css({left:0,top:-10000,'z-index':0});
@@ -287,6 +302,19 @@ function loadProp(p){
 	var d = propData[p];
 	points = 0;	propID = 0; propMode = true; firstPropPass = true;
 
+	//add prop header
+	$('<div/>', {
+		'class': 'propheader'
+	}).appendTo('#container');
+
+	$('.propheader').append(propTitles[propSelected].icon);
+	$('.propheader svg').css('fill', propTitles[propSelected].color)
+
+	$('<h2/>', {
+		'text': propTitles[propSelected].name 
+	}).appendTo('.propheader');
+
+
 	//build slides
 	for (var i = 0 ; i < d.length ; i++){
 		//check if obj exists
@@ -296,18 +324,19 @@ function loadProp(p){
 		if (!d[i].hasOwnProperty('victoryslide')){d[i]['victoryslide'] = false;}
 
 		//base template
+			
 			//base section
 			$('<section/>', {
 				'id': 		d[i].slideid,
 				'class': 	'relative startpos ' + p,
-			}).appendTo('#container');
+			}).appendTo('#container');	
 
-			//<div class="propprogress"><div><div></div></div></div>
 
 			//prop question shell
 			$('<div/>', {
-				'class': 'propquestion',
+				'class': 'propquestion'
 			}).appendTo('#' + d[i].slideid);
+
 
 			//prop question type
 			if (d[i].victoryslide == true){				
@@ -341,7 +370,6 @@ function loadProp(p){
 
 	//define the rightSlide as first slide
 	propSlide = d[0].slideid;
-
 }
 
 /* ANIMATION CONTROL
