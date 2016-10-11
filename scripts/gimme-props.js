2e6a7be25fa71694d562b3b54a19143617ea3ba1
John Osborn <bayreporta@gmail.com>
2016-10-10 23:56:39 -0700

    resource slide, header

--- scripts/gimme-props.js
+++ scripts/gimme-props.js
@@ -1,8 +1,8 @@
 /* GLOBAL VARIABLES
 --------------------------------------------------------*/
 var curSlide = '#nocookie', rightSlide = '#proplist', leftSlide = null, upSlide = null, downSlide = null, propSlide = null;
-var propSelected = null, propID, tutorial = false, lockSlide = false, propMode = false, returning, firstPropPass;
-var propData = new Object, points;
+var propSelected = null, propID, tutorial = false, lockSlide = false, propMode = false, returning, firstPropPass, points;
+var propData = new Object, resources = new Array;
 var propTitles = {
 	'prop59': {
 		name: 'Prop 59',
@@ -10,6 +10,7 @@ var propTitles = {
 	 	icon:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><rect x="5" y="5" width="90" height="90" fill="none"/><path d="M89.6 33.3c-2.2-5.1-5.3-9.7-9.2-13.7 -3.9-3.9-8.5-7-13.7-9.2C61.4 8.2 55.8 7.1 50 7.1s-11.4 1.1-16.7 3.4c-5.1 2.2-9.7 5.3-13.7 9.2 -3.9 3.9-7 8.5-9.2 13.7 -2.2 5.3-3.4 10.9-3.4 16.7 0 5.8 1.1 11.4 3.4 16.7 2.2 5.1 5.3 9.7 9.2 13.7 3.9 3.9 8.5 7 13.7 9.2 5.3 2.2 10.9 3.4 16.7 3.4s11.4-1.1 16.7-3.4c5.1-2.2 9.7-5.3 13.7-9.2s7-8.5 9.2-13.7c2.2-5.3 3.4-10.9 3.4-16.7C93 44.2 91.8 38.6 89.6 33.3zM87.5 65.9c-2 4.8-5 9.2-8.7 12.9 -3.7 3.7-8.1 6.7-12.9 8.7 -5 2.1-10.3 3.2-15.8 3.2 -5.5 0-10.8-1.1-15.8-3.2 -4.8-2-9.2-5-12.9-8.7 -3.7-3.7-6.7-8.1-8.7-12.9 -2.1-5-3.2-10.3-3.2-15.8s1.1-10.8 3.2-15.8c2-4.8 5-9.2 8.7-12.9 3.7-3.7 8.1-6.7 12.9-8.7C39.2 10.5 44.5 9.4 50 9.4s10.8 1.1 15.8 3.2c4.8 2 9.2 5 12.9 8.7 3.7 3.7 6.7 8.1 8.7 12.9 2.1 5 3.2 10.3 3.2 15.8S89.6 60.8 87.5 65.9z"/><path d="M40.6 37.8c0 4.2 2.7 7.8 6.7 9l0-5.7 -0.1-12.3C43.3 30.1 40.6 33.7 40.6 37.8z"/><path d="M52.7 71.2c4-1.2 6.7-4.8 6.7-9 0-4.2-2.7-7.8-6.6-9L52.7 70.4V71.2z"/><path d="M76.1 23.9c-3.4-3.4-7.3-6.1-11.7-7.9 -4.6-1.9-9.4-2.9-14.4-2.9s-9.8 1-14.4 2.9c-4.4 1.9-8.3 4.5-11.7 7.9 -3.4 3.4-6.1 7.3-7.9 11.7 -1.9 4.6-2.9 9.4-2.9 14.4 0 5 1 9.8 2.9 14.4 1.9 4.4 4.5 8.4 7.9 11.7 3.4 3.4 7.3 6.1 11.7 7.9 4.6 1.9 9.4 2.9 14.4 2.9s9.8-1 14.4-2.9c4.4-1.9 8.4-4.5 11.7-7.9 3.4-3.4 6.1-7.3 7.9-11.7 1.9-4.6 2.9-9.4 2.9-14.4 0-5-1-9.8-2.9-14.4C82.2 31.3 79.5 27.3 76.1 23.9zM52.8 41.2l0 6.4c3.3 0.6 6.3 2.4 8.5 5 2.3 2.7 3.6 6.2 3.6 9.7 0 3.6-1.3 7.1-3.6 9.8 -2.2 2.6-5.3 4.3-8.6 4.9v2.6c0 1.5-1.2 2.8-2.8 2.8s-2.8-1.2-2.8-2.8v-2.6c-3.3-0.6-6.3-2.4-8.5-5 -2.3-2.7-3.6-6.2-3.6-9.7 0-1.5 1.2-2.8 2.8-2.8s2.8 1.2 2.8 2.8c0 4.1 2.7 7.7 6.6 9V70.4l0.1-17.9c-3.3-0.6-6.3-2.4-8.6-5 -2.3-2.7-3.6-6.2-3.6-9.7 0-3.6 1.3-7 3.6-9.7 2.2-2.6 5.2-4.3 8.5-5l0-2.6c0-1.5 1.2-2.8 2.8-2.8h0c1.5 0 2.8 1.2 2.8 2.8l0 2.6c3.3 0.6 6.4 2.4 8.6 5 2.3 2.7 3.6 6.2 3.6 9.8 0 1.5-1.2 2.8-2.8 2.8s-2.8-1.2-2.8-2.8c0-4.2-2.7-7.8-6.6-9L52.8 41.2z"/></svg>'
 	}
 }
+var propPositions = new Array;
 
 /* COOKIE CONTROL
 --------------------------------------------------------*/
@@ -43,8 +44,7 @@ function initTransitions(){
 	$(document).on('keydown', function(event){
 		if (rightSlide != null && lockSlide == false){
 			if (event.which == 39){
-				console.log(curSlide)
-				console.log(rightSlide)
+				changeBackground();
 				$(curSlide).animate({
 					left:-10000
 				}, 300)
@@ -61,6 +61,7 @@ function initTransitions(){
 	$(document).on('keydown', function(event){
 		if (leftSlide != null && lockSlide == false){
 			if (event.which == 37){
+				changeBackground();
 				$(curSlide).animate({
 					left:10000
 				}, 300)
@@ -77,6 +78,7 @@ function initTransitions(){
 	$(document).on('keydown', function(event){
 		if (upSlide != null && lockSlide == false){
 			if (event.which == 38){
+				changeBackground();
 				$(curSlide).animate({
 					top:10000
 				}, 300)
@@ -93,6 +95,7 @@ function initTransitions(){
 	$(document).on('keydown', function(event){
 		if (downSlide != null && lockSlide == false){
 			if (event.which == 40){
+				changeBackground();
 				$(curSlide).animate({
 					top:-10000
 				}, 300)
@@ -108,7 +111,8 @@ function initTransitions(){
 	//swipe left
 	$(window).on('swipeleft', function(event){
 		if (rightSlide != null && lockSlide == false){
-				$(curSlide).animate({
+			changeBackground();
+			$(curSlide).animate({
 				left:-10000
 			}, 300)
 			$(rightSlide).animate({
@@ -122,7 +126,8 @@ function initTransitions(){
 	//swipe right
 	$(window).on('swiperight', function(event){
 		if (leftSlide != null && lockSlide == false){
-				$(curSlide).animate({
+			changeBackground();
+			$(curSlide).animate({
 				left:10000
 			}, 300)
 			$(leftSlide).animate({
@@ -137,6 +142,7 @@ function initTransitions(){
 	//swipe up
 	$(window).on('swipeup', function(event){
 		if (downSlide != null && lockSlide == false){
+			changeBackground();
 			$(curSlide).animate({
 				top:-10000
 			}, 300)
@@ -152,6 +158,7 @@ function initTransitions(){
 	//swipe down
 	$(window).on('swipedown', function(event){
 		if (upSlide != null && lockSlide == false){
+			changeBackground();
 			$(curSlide).animate({
 				top:10000
 			}, 300)
@@ -168,6 +175,17 @@ function initJSON(){
 	$.getJSON('scripts/json/prop59.json', function(data){
 		propData['prop59'] = data.prop59;
 	});
+
+	$.getJSON('scripts/json/resources.json', function(data){
+		propData['prop59'].resources = data[8];
+	});
+}
+
+/* GENERAL CONTROL
+--------------------------------------------------------*/
+function changeBackground(){
+	var color = $(curSlide).css('background-color');
+	$('html').css('background-color', color);
 }
 
 /* SLIDE CONTROL
@@ -231,11 +249,8 @@ function configureSlides(direct){
 	}
 	//RETURNING TO PROP
 	else if (returning == true){   
-		curSlide = '#proplist'; downSlide = '#prop59';returning = false;
-		setTimeout(function(){
-			$('.' + propSelected).add('.propheader').remove();
-		}, 300);
-		$('.downcaret').animate({opacity:.3}, 300); $('.propcaretleft').add('.propcaretright').fadeOut(300);
+		curSlide = '#resources'; leftSlide = '#proplist'; rightSlide = '#proplist'; returning = false;		
+		$('.rightcaret').add('.leftcaret').animate({opacity:.3}, 300); $('.propcaretleft').add('.propcaretright').fadeOut(300);
 	}
 	//ABOUT
 	else if (curSlide == '#intro' && direct == 'right' || curSlide == '#whoweare' && direct == 'up'){    
@@ -269,10 +284,16 @@ function configureSlides(direct){
 		lockSlide = true;
 		dropCookie('no');
 	}
-	//PROP LIST MAIN
-	else if (curSlide == '#yescookie' && direct == 'left' || curSlide == '#nocookie' && direct == 'right' || curSlide == '#prop51' && direct == 'up'){   
+	//PROP LIST MAIN _____________________________________________________________________//
+
+	else if (curSlide == '#resources' || curSlide == '#yescookie' && direct == 'left' || curSlide == '#nocookie' && direct == 'right' || curSlide == '#prop51' && direct == 'up'){   
 		clearAnimations(); curSlide = '#proplist'; downSlide = '#prop51'; tutorial = false;
+		$('#menu').fadeIn(300);
+		setTimeout(function(){
+			$('.' + propSelected).add('.propheader').remove();
+		}, 300);
 		$('.downcaret').animate({opacity:.3}, 300); $('.rightcaret').add('.leftcaret').add('.upcaret').animate({opacity:0}, 300);
+
 	}
 	//TUTORIAL MASTER
 	else if (tutorial == true && direct == 'right'){ 
@@ -280,15 +301,80 @@ function configureSlides(direct){
 		$('.rightcaret').animate({opacity:.3}, 300); $('.leftcaret').add('.upcaret').add('.downcaret').animate({opacity:0}, 300);
 	}
 	//PROP LIST 51
-	else if (curSlide == '#proplist' && direct == 'down' || curSlide == '#prop59' && direct == 'up'){   
+	else if (curSlide == '#proplist' && direct == 'down' || curSlide == '#prop52' && direct == 'up' || curSlide == '#prop65' && direct == 'down'){   
 		clearAnimations(); dropHammer(true);
-		curSlide = '#prop51'; upSlide = '#proplist'; downSlide = '#prop59';rightSlide = '#tutorial'; propSelected = 'prop51';tutorial = true;
+		curSlide = '#prop51'; upSlide = '#prop65'; downSlide = '#prop52'; propSelected = 'prop51'; tutorial = true;
 		$('.downcaret').add('.upcaret').add('.rightcaret').animate({opacity:.3}, 300); $('.leftcaret').animate({opacity:0}, 300);
 	}	
+	//PROP LIST 52
+	else if (curSlide == '#prop51' && direct == 'down' || curSlide == '#prop53' && direct == 'up'){   
+		clearAnimations(); moveCoins(true);
+		curSlide = '#prop52'; upSlide = '#prop51'; downSlide = '#prop53'; propSelected = 'prop52'; tutorial = true;
+	}	
+	//PROP LIST 53
+	else if (curSlide == '#prop52' && direct == 'down' || curSlide == '#prop54' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop53'; upSlide = '#prop52'; downSlide = '#prop54'; propSelected = 'prop53'; tutorial = true;
+	}	
+	//PROP LIST 54
+	else if (curSlide == '#prop53' && direct == 'down' || curSlide == '#prop55' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop54'; upSlide = '#prop53'; downSlide = '#prop55'; propSelected = 'prop54'; tutorial = true;
+	}	
+	//PROP LIST 55
+	else if (curSlide == '#prop54' && direct == 'down' || curSlide == '#prop56' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop55'; upSlide = '#prop54'; downSlide = '#prop56'; propSelected = 'prop55'; tutorial = true;
+	}	
+	//PROP LIST 56
+	else if (curSlide == '#prop55' && direct == 'down' || curSlide == '#prop57' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop56'; upSlide = '#prop55'; downSlide = '#prop57'; propSelected = 'prop56'; tutorial = true;
+	}	
+	//PROP LIST 57
+	else if (curSlide == '#prop56' && direct == 'down' || curSlide == '#prop58' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop57'; upSlide = '#prop56'; downSlide = '#prop58'; propSelected = 'prop57'; tutorial = true;
+	}	
+	//PROP LIST 58
+	else if (curSlide == '#prop57' && direct == 'down' || curSlide == '#prop59' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop58'; upSlide = '#prop57'; downSlide = '#prop59'; propSelected = 'prop58'; tutorial = true;
+	}	
 	//PROP LIST 59
-	else if (curSlide == '#prop51' && direct == 'down'){   
-		rainingCoins();
-		curSlide = '#prop59'; upSlide = '#prop51'; rightSlide = '#tutorial'; propSelected = 'prop59';tutorial = true;
+	else if (curSlide == '#prop58' && direct == 'down' || curSlide == '#prop60' && direct == 'up'){   
+		clearAnimations(); rainingCoins(); 
+		curSlide = '#prop59'; upSlide = '#prop58'; downSlide = '#prop60'; rightSlide = '#tutorial'; propSelected = 'prop59';tutorial = true;
+	}	
+	//PROP LIST 60
+	else if (curSlide == '#prop59' && direct == 'down'  || curSlide == '#prop61' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop60'; upSlide = '#prop59'; downSlide = '#prop61'; propSelected = 'prop60';tutorial = true;
+	}	
+	//PROP LIST 61
+	else if (curSlide == '#prop60' && direct == 'down'  || curSlide == '#prop62' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop61'; upSlide = '#prop60'; downSlide = '#prop62'; propSelected = 'prop61';tutorial = true;
+	}	
+	//PROP LIST 62 AND 66
+	else if (curSlide == '#prop61' && direct == 'down'  || curSlide == '#prop63' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop62'; upSlide = '#prop61'; downSlide = '#prop63'; propSelected = 'prop62';tutorial = true;
+	}
+	//PROP LIST 63
+	else if (curSlide == '#prop62' && direct == 'down'  || curSlide == '#prop64' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop63'; upSlide = '#prop62'; downSlide = '#prop64'; propSelected = 'prop63';tutorial = true;
+	}	
+	//PROP LIST 64
+	else if (curSlide == '#prop63' && direct == 'down'  || curSlide == '#prop65' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop64'; upSlide = '#prop63'; downSlide = '#prop65'; propSelected = 'prop64';tutorial = true;
+	}	
+	//PROP LIST 65 AND 67
+	else if (curSlide == '#prop64' && direct == 'down'  || curSlide == '#prop51' && direct == 'up'){   
+		clearAnimations();
+		curSlide = '#prop65'; upSlide = '#prop64'; downSlide = '#prop51'; propSelected = 'prop65';tutorial = true;
 		$('.downcaret').add('.upcaret').add('.rightcaret').animate({opacity:.3}, 300); $('.leftcaret').animate({opacity:0}, 300);
 	}	
 
@@ -307,6 +393,11 @@ function calcPoints(d, direct){
 function loadProp(p){
 	var d = propData[p];
 	points = 0;	propID = 0; propMode = true; firstPropPass = true;
+
+	//populate resource slide
+	$('#resources a:eq(0)').attr('href', d.resources.kqed);
+	$('#resources a:eq(1)').attr('href', d.resources.ballotfyi);
+	$('#resources a:eq(2)').attr('href', d.resources.ballotpedia);
 
 	//add prop header
 	$('<div/>', {
@@ -381,8 +472,28 @@ function loadProp(p){
 /* ANIMATION CONTROL
 --------------------------------------------------------*/
 function clearAnimations(){
-	dropHammer(false)
+	dropHammer(false);
+	moveCoins(false);
 	destroyCoins();
+}
+
+function moveCoins(t){
+	if (t == true){
+		$('#prop52>div[data="coin"]').css({
+			'-webkit-animation-play-state': 'running',
+			'-ms-animation-play-state': 'running',
+			'-moz-animation-play-state': 'running',
+			'animation-play-state': 'running'
+		});
+	}
+	else {
+		$('#prop52>div[data="coin"]').css({
+			'-webkit-animation-play-state': 'paused',
+			'-ms-animation-play-state': 'paused',
+			'-moz-animation-play-state': 'paused',
+			'animation-play-state': 'paused'
+		});
+	}
 }
 
 function dropHammer(t){
